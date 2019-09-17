04e52194c13614297873fb75085de568ceb749da
trent <trent@DESKTOP-NKK8L7L>
2019-09-16 19:43:49 -0500

    Merge Trenton/master
    Conflicts:
    	src/main/java/salesTax.java

---
+++ src/main/java/com/mycompany/lab2/sumOfNumbers.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author trent
+ */
+public class sumOfNumbers {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the number you are going to sum");
+        double numToSum = keyboard.nextDouble();
+        double numToCount = 1;
+        
+          /*  
+        do while (numToCount <= numToSum)
+            
+        
+         */
+    }
+}
