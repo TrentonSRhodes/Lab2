cdef2e258dcfd25167d7b63dfd6860b90c3f405c
trent <trent@DESKTOP-NKK8L7L>
2019-09-17 19:33:22 -0500

    Merge Trenton/master
    Conflicts:
    	src/main/java/salesTax.java

---
+++ src/main/java/com/mycompany/lab2/barChart.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author trent
+ */
+public class barChart {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the sales for the five stores");
+        double storeOne = keyboard.nextDouble();
+        double storeTwo = keyboard.nextDouble();
+        double storeThree = keyboard.nextDouble();
+        double storeFour = keyboard.nextDouble();
+        double storeFive = keyboard.nextDouble();
+        
+        
+    }
+}
